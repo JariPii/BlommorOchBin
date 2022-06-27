30dac0b17325dcacfbd59b4667b4e00e7f67e7ac
JariPii <89419494+JariPii@users.noreply.github.com>
2022-06-27 14:50:33 +0200

    Start

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useMemo, useEffect } from 'react';
+
+import { StyleSheet, Text, TouchableOpacity, SafeAreaView } from 'react-native';
+import { Picker, onOpen } from 'react-native-actions-sheet-picker';
+
+/*
+ **Example data:
+ */
+import countries from './materials/countries.json';
+import materials from './materials/materials.json';
 
 export default function App() {
+  const [data, setData] = useState([]);
+  const [selected, setSelected] = useState(undefined);
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    setData(materials);
+  }, []);
+
+  /*
+   **Example filter function
+   * @param {string} filter
+   */
+  const filteredData = useMemo(() => {
+    if (data && data.length > 0) {
+      return data.filter((item) =>
+        item.name
+          .toLocaleLowerCase('en')
+          .includes(query.toLocaleLowerCase('en'))
+      );
+    }
+  }, [data, query]);
+
+  /*
+   **Input search
+   *@param {string} text
+   */
+  const onSearch = (text) => {
+    setQuery(text);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          onOpen('country');
+        }}
+      >
+        <Text>Open ActionSheet</Text>
+      </TouchableOpacity>
+      <Text style={{ padding: 10 }}>Chosen : {JSON.stringify(selected)}</Text>
+      <Picker
+        id="country"
+        data={filteredData}
+        inputValue={query}
+        searchable={true}
+        label="Select Country"
+        setSelected={setSelected}
+        onSearch={onSearch}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: '#8B93A5',
+    padding: 10,
+    borderRadius: 6,
+    marginTop: 50,
   },
 });
